80a1c60d2e17da3f428f3c343fca565993b9eaee
Matieu <hei.tafita.2@gmail.com>
2025-08-30 09:42:09 +0300

    chore(client): regenerate SDK

---
+++ client/src/api/models/PublicUser.ts
@@ -0,0 +1,13 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type PublicUser = {
+    user_id: number;
+    email: string;
+    username?: string | null;
+    firstname?: string | null;
+    lastname?: string | null;
+    created_at: string;
+};
+
