7d8b368763053ec752a087f03f01ad5f5cbbc4c4
colombefioren <colomberakotonjanahary@gmail.com>
2025-09-05 16:48:10 +0300

    style: higher margin the more little the viewport is

--- client/src/pages/Incomes.tsx
+++ client/src/pages/Incomes.tsx
@@ -86,7 +86,7 @@ export const Incomes = () => {
 
   return (
     <>
-      <div className="relative z-2 mb-10 mt-30 mx-auto text-light max-w-7xl px-6">
+      <div className="relative z-2 mb-10 mt-30 mx-auto text-light max-w-7xl px-6 lg:ml-20">
         <IncomeHeader totalIncome={totalIncome} onNewIncome={handleNewIncome} />
 
         <StatsCards
