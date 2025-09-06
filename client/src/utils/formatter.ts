33dec3f7f3ea1b1913cd0565a7195be578f06f4a
Jonathan <rakotoarisoajonathan0@gmail.com>
2025-09-06 11:50:00 +0300

    chore: delete unused formatter.ts file

--- client/src/utils/formatter.ts
+++
@@ -1,7 +0,0 @@
-export function formatMonth(ym: string) {
-  const [year, month] = ym.split("-");
-  return new Date(Number(year), Number(month) - 1).toLocaleDateString("en-En", {
-    month: "short",
-    year: "numeric",
-  });
-}
