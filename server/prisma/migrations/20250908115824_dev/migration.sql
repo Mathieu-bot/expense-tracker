95b9addee41e87f6a6adfd7d8649569bfa947c72
Jonathan <rakotoarisoajonathan0@gmail.com>
2025-09-08 14:59:12 +0300

    feat(Prisma): add Receipt table to the Prisma schema

--- server/prisma/migrations/20250908115824_dev/migration.sql
+++ server/prisma/migrations/20250908115824_dev/migration.sql
@@ -33,12 +33,12 @@ CREATE TABLE "public"."expense" (
     "expense_id" SERIAL NOT NULL,
     "amount" DOUBLE PRECISION NOT NULL,
     "description" VARCHAR(2000),
-    "type" VARCHAR(20) NOT NULL DEFAULT 'ONE_TIME',
-    "receipt_upload" VARCHAR(2000),
+    "type" "public"."ExpenseType" NOT NULL DEFAULT 'ONE_TIME',
     "creation_date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "expense_date" TIMESTAMP(3),
     "start_date" TIMESTAMP(3),
     "end_date" TIMESTAMP(3),
+    "last_processed" TIMESTAMP(3),
     "user_id" INTEGER NOT NULL,
     "category_id" INTEGER NOT NULL,
 
@@ -58,8 +58,36 @@ CREATE TABLE "public"."income" (
     CONSTRAINT "income_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "public"."receipt" (
+    "receipt_id" SERIAL NOT NULL,
+    "user_id" INTEGER,
+    "expense_id" INTEGER NOT NULL,
+    "filename" TEXT NOT NULL,
+    "mime_type" VARCHAR(50) NOT NULL,
+    "size" INTEGER NOT NULL,
+    "bytes" BYTEA NOT NULL,
+    "sha256" TEXT NOT NULL,
+    "creation_date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "receipt_pkey" PRIMARY KEY ("receipt_id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "user_email_key" ON "public"."user"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "receipt_expense_id_key" ON "public"."receipt"("expense_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "receipt_sha256_key" ON "public"."receipt"("sha256");
+
+-- CreateIndex
+CREATE INDEX "receipt_user_id_idx" ON "public"."receipt"("user_id");
+
+-- CreateIndex
+CREATE INDEX "receipt_mime_type_idx" ON "public"."receipt"("mime_type");
 
 -- AddForeignKey
 ALTER TABLE "public"."expense_category" ADD CONSTRAINT "expense_category_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "public"."user"("user_id") ON DELETE CASCADE ON UPDATE CASCADE;
@@ -72,3 +100,9 @@ ALTER TABLE "public"."expense" ADD CONSTRAINT "expense_category_id_fkey" FOREIGN
 
 -- AddForeignKey
 ALTER TABLE "public"."income" ADD CONSTRAINT "income_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "public"."user"("user_id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."receipt" ADD CONSTRAINT "receipt_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "public"."user"("user_id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."receipt" ADD CONSTRAINT "receipt_expense_id_fkey" FOREIGN KEY ("expense_id") REFERENCES "public"."expense"("expense_id") ON DELETE CASCADE ON UPDATE CASCADE;
